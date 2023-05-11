e960dd73c5a059de8eeb725a01dc871d624ac4a4
Vivian Sunmola <viviansunmola@gmail.com>
2023-05-11 14:36:45 +0100

    Set up and maintained between a server and a client machine

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host,port))
+
+    message = input("You are connected to Server ${port} Please enter message")
+    while message != 'q':
+        s.send(message.encode('utf-8'))
+
+        data = s.recv(1024).decode('utf-8')
+        print ("Messaged recied from server " + str(data))
+        message = input("->")
+    s.close()
+
+    if __name__== '__main__':
+        Main()
